5850ac491a7bb83d94dc54be2bfa9e69099e901e
3a713220 <48341322+3a713220@users.noreply.github.com>
2019-05-03 09:05:58 +0800

    Add files via upload

---
+++ 作業10+.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) 
+{
+	
+	int num,i;
+	
+	printf("請輸入一個數字:");
+	scanf("%d",&num);
+	
+	printf("此數字的質因數為");
+	i=2; 
+	
+	while(num>1){
+	    while(num%i==0){
+	    	printf("%d ",i);
+	    	num=num/i;
+		}	
+		i++;
+	}
+	printf("\n");
+	
+	system("PAUSE");
+	return 0;
+}
